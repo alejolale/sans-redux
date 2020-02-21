934f0b3b28498ece48bb8e6580d2e02cc4223077
sergio <sergio>
2020-02-21 12:56:20 +0100

    removes unused component file

--- src/App.js
+++ src/App.js
@@ -10,13 +10,8 @@ const App = () => {
 
   return (
     <div className="App">
-<<<<<<< HEAD
       <NavBarContainer />
       <LightContainer />
-=======
-      <NavBar />
-      <LightComponent />
->>>>>>> 958a3b856da5a536d75d3113155abb93bb67b3b5
     </div>
   );
 }
--- src/lightReducer.js
+++ src/lightReducer.js
@@ -1,10 +1,6 @@
 // actions possibles =>
 // { type: 'SWITCH' }
-<<<<<<< HEAD
-const lightReducer = (state = 'onnn', action) => {
-=======
 const lightReducer = (state = 'on', action) => {
->>>>>>> 958a3b856da5a536d75d3113155abb93bb67b3b5
     switch(action.type){
         case 'SWITCH':
             return state === 'on' ? 'off' : 'on';
