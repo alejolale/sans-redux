108ec1f000204d31293aee71c31ae002354faf84
sergio <sergio>
2020-02-21 11:53:24 +0100

    create reducers and store, no state to pass

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// 1 - import de store redux et le provider
+import { createStore } from 'redux';
+import  { Provider } from 'react-redux';
+
+// 2 - import  reducer
+import lightReducer from './lightReducer';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// 3 - creation of the store
+const store = createStore(lightReducer);
+
+// 4 -  Add the provider into the app & propertie of the store
+ReactDOM.render(<Provider store={store} ><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
